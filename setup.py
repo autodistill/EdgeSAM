542494ee0108b44b38a08ef74f12f3164288e86b
James <37276661+capjamesg@users.noreply.github.com>
2023-12-12 16:44:31 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="edge_sam_segment_anything",
+    name="segment_anything",
     version="1.0",
     install_requires=[],
     packages=find_packages(exclude="notebooks"),
